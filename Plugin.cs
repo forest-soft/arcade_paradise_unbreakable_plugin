679d338e6a26857761b90ed92b7b1806c9fc41b4
森林ソフト <kimoto@forest-soft.net>
2023-09-03 18:30:35 +0900

    実装

--- Plugin.cs
+++ Plugin.cs
@@ -1,15 +1,39 @@
 ﻿using BepInEx;
 using BepInEx.IL2CPP;
+using HarmonyLib;
+using System.Reflection;
 
 namespace arcade_paradise_unbreakable_plugin
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("ArcadeParadise.exe")]
     public class Plugin : BasePlugin
     {
         public override void Load()
         {
-            // Plugin startup logic
-            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            var assembly = Assembly.GetExecutingAssembly();
+            harmony.PatchAll(assembly);
+            harmony.PatchAll();
+        }
+
+        // 壊れたゲーム機を修理するパッチ
+        [HarmonyPatch(typeof(RAT.Scriptables.Objects.ArcadeMachine), nameof(RAT.Scriptables.Objects.ArcadeMachine.IsBroken), MethodType.Getter)]
+        public class RAT_Scriptables_Objects_ArcadeMachine_get_IsBroken_Patch
+        {
+            static void Postfix(ref bool __result, RAT.Scriptables.Objects.ArcadeMachine __instance)
+            {
+                if (__result)
+                {
+                    // 壊れている場合は修理する。
+                    RAT.Arcade.ArcadeMachine broken_arcade_machine = RAT.Managers.ArcadeMachineManager.GetMachineByDataID(__instance.ID);
+                    if (broken_arcade_machine != null)
+                    {
+                        broken_arcade_machine.MachineFixed();
+                        __result = false;
+                    }
+                }
+            }
         }
     }
 }
